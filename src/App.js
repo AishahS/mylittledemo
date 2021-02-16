b7802db81a2d138ac9faca3f5b437c7449672820
AishahS <aishahalsaqan@gmail.com>
2021-02-16 11:05:18 +0300

    Modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <text
+        style={{
+          fontSize: 48,
+          fontWeight: "bold",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        SNAPZO
+      </text>
+      <text
+        style={{
+          fontSize: 28,
+          fontWeight: "bold",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        ITS A PHOTOGRAPHER APP
+      </text>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "10px",
+        }}
+      >
+        <img
+          src="https://clipground.com/images/camera-lens-logo-design-clipart-2.png"
+          height={500}
+          width={1000}
+        ></img>
+
+        {/* <header className="App-header">
+        <a className="App-link" rel="noopener noreferrer">
+          SNAPZO
         </a>
-      </header>
-    </div>
+        <a>
+          <p>Ann photographer app!</p>
+        </a>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        {data.map(({ name, price, image }) => (
+          <>
+            <img className={name} src={image} alt="logo"></img>
+            <p>{name}</p>
+            <p>{price}</p>
+          </>
+        ))}
+      </header> */}
+      </div>
+    </>
   );
 }
 
