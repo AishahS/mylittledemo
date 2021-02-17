be6d24e1c50992ca898723e26a6a907e8ef2bb82
AishahS <aishahalsaqan@gmail.com>
2021-02-17 18:38:43 +0300

    TASK 3 IS DONE

--- mylittledemo/src/products.js
+++ mylittledemo/src/products.js
@@ -2,14 +2,14 @@ export const products = [
   {
     name: "Camera Lens",
     id: "1",
-    price: "40$",
+    price: "80KD",
     image:
       "https://www.symbios.pk/image/cache/data/2/2137_AF_NIKKOR_50mm_f-1_8D-220x220.png",
   },
   {
     name: "Tripod Stand",
     id: "2",
-    price: "50$",
+    price: "35KD",
     image:
       "https://lh3.googleusercontent.com/proxy/Rm4KcW5opBsQbSKbACbE5Um0ZnXJjuBgdgOyC1ECytWazOzd1tyBEYiSKtZxHiy-lyOXiVH64prPz6kve_TcE2Bs0fksdymLxeig78_OqUHjgQndvq4rQQ",
   },
