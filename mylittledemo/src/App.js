ecbcd12d5f980115a7c485540008b6564288442b
AishahS <aishahalsaqan@gmail.com>
2021-02-17 21:27:19 +0300

    task 2 is done

--- mylittledemo/src/App.js
+++ mylittledemo/src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
+import { products } from "./products";
 import { styles } from "./style";
-import Home from "./components/Home";
-import productList from "./components/ProductList";
 
 function App() {
+  const productList = products.map((product) => {
+    return (
+      <div style={styles.productContainer}>
+        <img src={product.image} width={200} height={200} />
+        <text style={styles.productName}>{product.name}</text>
+        <text>{product.price}</text>
+      </div>
+    );
+  });
   return (
     <>
-      <Home />
+      <text style={styles.title}>SNAPZO</text>
+      <text style={styles.tagLine}>Ann photographer app</text>
+      <div style={styles.imageContainer}>
+        <img
+          src="https://iso.500px.com/wp-content/uploads/2016/03/pedroquintela-1500x844.jpg"
+          height={500}
+          width={1000}
+        ></img>
+      </div>
       <div style={styles.productList}>{productList}</div>
     </>
   );
--- mylittledemo/src/products.js
+++ mylittledemo/src/products.js
@@ -2,15 +2,15 @@ export const products = [
   {
     name: "Camera Lens",
     id: "1",
-    price: "80KD",
+    price: "40KD",
     image:
       "https://www.symbios.pk/image/cache/data/2/2137_AF_NIKKOR_50mm_f-1_8D-220x220.png",
   },
   {
     name: "Tripod Stand",
     id: "2",
-    price: "35KD",
+    price: "85KD",
     image:
-      "https://lh3.googleusercontent.com/proxy/Rm4KcW5opBsQbSKbACbE5Um0ZnXJjuBgdgOyC1ECytWazOzd1tyBEYiSKtZxHiy-lyOXiVH64prPz6kve_TcE2Bs0fksdymLxeig78_OqUHjgQndvq4rQQ",
+      "https://www.noelleeming.co.nz/shop/content/images/products/150/150816.jpg",
   },
 ];
